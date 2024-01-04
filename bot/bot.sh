80d3c4e2f158eeaf6ba74821eb793ff5167d53ca
TIKUSMERDEKA <147392949+Tikusmerdeka@users.noreply.github.com>
2024-01-04 22:12:17 +0700

    Add files via upload

--- bot/bot.sh
+++ bot/bot.sh
@@ -1,4 +1,4 @@
-<#!/bin/bash
+#!/bin/bash
 NS=$( cat /etc/xray/dns )
 PUB=$( cat /etc/slowdns/server.pub )
 domain=$(cat /etc/xray/domain)
@@ -9,28 +9,28 @@ NC='\e[0m'
 apt update && apt upgrade
 apt install python3 python3-pip git
 cd /usr/bin
-wget https://raw.githubusercontent.com/Tikusmerdeka/Apex/main/bot/bot.zip
+wget https://raw.githubusercontent.com/gemilangkinasih/Apex/ZX/bot/bot.zip
 unzip bot.zip
 mv bot/* /usr/bin
 chmod +x /usr/bin/*
 rm -rf bot.zip
 clear
-wget https://raw.githubusercontent.com/Tikusmerdeka/Apex/main/bot/kyt.zip
+wget https://raw.githubusercontent.com/gemilangkinasih/Apex/ZX/bot/kyt.zip
 unzip kyt.zip
 pip3 install -r kyt/requirements.txt
 
 clear
 echo ""
-echo -e "\033[1;96m┌──────────────────────────────────────────┐\033[0m"
+echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e " \e[1;97;101m          ADD BOT PANEL          \e[0m"
-echo -e "\033[1;96m└──────────────────────────────────────────┘\033[0m"
-echo -e "${grenbo}Tutorial Create Bot and Telegram ID${NC}"
-echo -e "${grenbo}➸ Create Bot and Get Token: @BotFather${NC}"
-echo -e "${grenbo}➸ Telegram ID Info: @MissRose_bot, command: /info${NC}"
-echo -e "\033[1;96m└──────────────────────────────────────────┘\033[0m"
+echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "${grenbo}Tutorial Creat Bot and ID Telegram${NC}"
+echo -e "${grenbo}[*] Creat Bot and Token Bot : @BotFather${NC}"
+echo -e "${grenbo}[*] Info Id Telegram : @MissRose_bot , perintah /info${NC}"
+echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e ""
-read -e -p "Input token ☞ " bottoken
-read -e -p "Input Id tele ☞" admin
+read -e -p "[*] Input your Bot Token : " bottoken
+read -e -p "[*] Input Your Id Telegram :" admin
 echo -e BOT_TOKEN='"'$bottoken'"' >> /usr/bin/kyt/var.txt
 echo -e ADMIN='"'$admin'"' >> /usr/bin/kyt/var.txt
 echo -e DOMAIN='"'$domain'"' >> /usr/bin/kyt/var.txt
